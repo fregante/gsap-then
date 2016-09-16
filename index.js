0fb1fd992ca9b59a2ec1aca92f94c8a99208fd18
Federico Brigante <bfred-it@users.noreply.github.com>
2016-09-16 11:48:30 +0700

    Keep context and arguments on existing callbacks

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.com.greensock.core.Animation.prototype.then = function (onFullfilled) {
 	return new Promise(resolve => {
 		const existing = this.eventCallback('onComplete');
-		this.eventCallback('onComplete', () => {
+		this.eventCallback('onComplete', function () {
 			if (existing) {
-				existing();
+				existing.apply(this, arguments);
 			}
 			resolve();
 		});
